e54d225f409b6c6a0da5955500e0abdb1873cfe5
Bruno Gomes <bruno.gurgel@ifrn.edu.br>
2026-06-02 15:50:43 -0300

    Formulário reativo adicionado no form de média

--- src/app/calcula-media/calcula-media.ts
+++ src/app/calcula-media/calcula-media.ts
@@ -1,22 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, 
+  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-calcula-media',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './calcula-media.html',
   styleUrl: './calcula-media.scss',
 })
 export class CalculaMedia {
   protected mediaParcial: number | undefined
-  
+  protected mediaFinal: number | undefined
+  #fb = inject(FormBuilder) //Ajuda a criar o objeto FormGroup
+  //grupo de controles de formulário relacionados (inputs, selects...)
+  protected formMedia: FormGroup; 
   constructor() {
+    this.formMedia = this.#fb.group({
+      bim1: [undefined, [Validators.required, 
+        Validators.min(0), Validators.max(100)]],
+      bim2: [undefined, [Validators.required, 
+        Validators.min(0), Validators.max(100)]],
+      bim3: [undefined, [Validators.required, 
+        Validators.min(0), Validators.max(100)]],
+      bim4: [undefined, [Validators.required, 
+        Validators.min(0), Validators.max(100)]]
+    })
     this.mediaParcial = undefined
+    this.mediaFinal = undefined 
   }
   
 
-  calcularMediaParcial(b1: number, b2: number, 
-                       b3: number, b4: number) {
+  calcularMediaParcial() {
+      const { bim1, bim2, bim3, bim4 } = this.formMedia.value
       this.mediaParcial = 
-        (b1 * 2 + b2 * 2 + b3 * 3 + b4 * 3) / 10
+        (bim1 * 2 + bim2 * 2 + bim3 * 3 + bim4 * 3) / 10
   }
+
+  //Método para calcular a média final. Recebe a nota 
 }
